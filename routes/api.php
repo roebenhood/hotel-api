75183ddd4581a28d21605b6ce42c38fb64e48bea
Jon <joncaesar@gmail.com>
2023-03-02 09:52:11 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RoomsController;
 use App\Http\Controllers\Api\EmployeeController;
 
 /*
@@ -19,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// START: Employees API
 
 Route::get('employees', [EmployeeController::class, 'getAllEmployees']);
 Route::post('employees', [EmployeeController::class, 'addNewEmployee']);
@@ -27,3 +29,12 @@ Route::get('employees/{id}/update',[EmployeeController::class, 'editEmployee']);
 Route::put('employees/{id}/update',[EmployeeController::class, 'updateEmployee']);
 Route::delete('employees/{id}/delete',[EmployeeController::class, 'deleteEmployee']);
 Route::post('employees/login', [EmployeeController::class, 'employeeLogin']);
+
+// END: Employees API
+
+// START: Rooms API
+
+Route::get('rooms', [RoomsController::class, 'getAllRooms']);
+Route::post('rooms', [RoomsController::class, 'addNewRoom']);
+
+// END: Rooms API
